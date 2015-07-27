b5518ff495c739c05c519c604d6d53ad6093af99
Mathew Kleppin <matkle414@gmail.com>
2015-07-26 19:21:26 -0500

    fix config errors

--- src/templation.js
+++ src/templation.js
@@ -1,5 +1,3 @@
-process.env.SUPPRESS_NO_CONFIG_WARNING = 'y';
-var config        = require('config');
 var fs            = require("fs");
 var path          = require("path");
 var _             = require("lodash");
@@ -22,29 +20,28 @@ function Templation(options){
     generateTextFromHTML: true
   };
 
-  config.util.extendDeep(defaultOptions, options);
-  config.util.setModuleDefaults('Templation', defaultOptions);
+  this.options = _.assign({}, defaultOptions, options);
 
 }
 
 Templation.prototype.send = function send(data, callback){
 
   //Get required data to generate HTML
-  var template = data.template || config.get('Templation.defaultTemplate');
+  var template = data.template || this.options.defaultTemplate;
 
   var html = this._getHtml(data.messageData, template);
   var attachments = this._getAttachments(html);
 
   var emailData = {
     to: data.to.name + " <" + data.to.email + ">",
-    from: data.from || config.get('Templation.from'),
+    from: data.from || this.options.from,
     subject: data.subject,
     html: html,
-    generateTextFromHTML: config.get('Templation.generateTextFromHTML'),
+    generateTextFromHTML: this.options.generateTextFromHTML,
     attachments: attachments
   };
 
-  var transportOptions = data.transportOptions || config.get('Templation.transportOptions');
+  var transportOptions = data.transportOptions || this.options.transportOptions;
 
   return this._getTransport(transportOptions).sendMail(emailData, callback);
 
@@ -54,16 +51,16 @@ Templation.prototype._getHtml = function(data, templateName){
 
   //Resolve our template path using the path of a template given in options,
   //or if the path is the template name itself
-  var templatePath = config.get('Templation.templates')[templateName] || templateName;
+  var templatePath = this.options.templates[templateName] || templateName;
   var templateContent = fs.readFileSync(templatePath, 'utf8');
 
   //Return the rendered version of our template via lodash template method.
-  return _.template(templateContent, config.get('Templation.templateOptions'))(data);
+  return _.template(templateContent, this.options.templateOptions)(data);
 };
 
 Templation.prototype._getAttachments = function(html){
   var attachments = [];
-  this._attachments = config.get('Templation.attachments');
+  this._attachments = this.options.attachments;
 
   //Go through each attachment in our options and find out which ones our template is using
   _.forEach(this._attachments, function(attachment){
